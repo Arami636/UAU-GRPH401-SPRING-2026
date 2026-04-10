4d55ba0689671f38103733234067f3d9eaf761c6
Avi Ramirez <abigailramirez636@gmail.com>
2026-04-10 18:28:46 -0500

    cannot see the buttons change anything?

--- week10_javascript101/scripts/light-dark.js
+++ week10_javascript101/scripts/light-dark.js
@@ -1,11 +1,10 @@
-function toggleDarkMode(){
-const body = document.querySelector("body");
-
-const btn = document.querySelector("toggleStn");
-
-body.classList.toggle("dark-mode");
-if (body.classList.contains("dark-mode")){
-  btn.textContent = "Switch to Light Mode";
-} else {btn.textContent = "Switch to Dark Mode";
-}
+function toggleDarkMode() {
+  const body = document.querySelector("body");
+  const btn = document.querySelector("toggleStn");
+  body.classList.toggle("dark-mode");
+  if (body.classList.contains("dark-mode")) {
+    btn.textContent = "Switch to Light Mode";
+  } else {
+    btn.textContent = "Switch to Dark Mode";
+  }
 }
